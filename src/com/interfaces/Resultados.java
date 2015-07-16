b7aa877e54ecc9579113f374780b2f21a41db26c
edgar-ach <edgar.acurio@gmail.com>
2015-07-16 17:14:54 -0500

    preparando interfaces

---
+++ src/com/interfaces/Resultados.java
@@ -0,0 +1,61 @@
+package com.interfaces;
+
+import java.awt.EventQueue;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import javax.swing.border.EmptyBorder;
+
+public class Resultados extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Resultados frame = new Resultados();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Resultados() {
+		setTitle("EXSHE - INFORMES");
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				cerrarInformes();
+			}
+		});
+		
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		setBounds(100, 100, 1366, 730);		
+		this.setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+	}
+	
+	public void cerrarInformes(){
+		
+			dispose();
+		
+	}
+	
+
+}
